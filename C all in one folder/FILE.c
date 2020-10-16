b942aede5ab7ba74971d03d491753c6388c722f3
Sayed-Anawer <sayedanawer99@gmail.com>
2020-10-16 13:38:09 +0600

    refactor: Add fputc for write in our text file.

--- C all in one folder/FILE.c
+++ C all in one folder/FILE.c
@@ -4,14 +4,20 @@ int main()
     FILE *file;
 
     file = fopen("file.txt","w");
+    char myName[] = "Md.Sayed Anawer Shawon.\n Nazmul Hossain Shohag.";
+    int len= strlen(myName);
 
     if(file == NULL)
     {
         printf("File is not open.");
     }
     else
-    {
-        printf("File is open.");
+    {   printf("File is open");
+
+        for (int i=0; i <len; i++)
+        {
+            fputc(myName[i],file);
+        }
         fclose(file);
     }
 }
