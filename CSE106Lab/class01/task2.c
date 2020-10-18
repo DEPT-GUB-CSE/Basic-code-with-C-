5ff23e01ab792da8fdf759437817a5b00dad6578
Sayed-Anawer <sayedanawer99@gmail.com>
2020-10-18 12:32:49 +0600

    refactor: task2 not properly done yet

---
+++ CSE106Lab/class01/task2.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+
+int st[100],size,top=-1,st2[100];
+
+void push(int element)
+{
+    if ( top == size -1 )
+    {
+        printf("Sorry ! Stack is full.Overflow Occur %d\n",element);
+    }
+    else
+    {
+        top = top+1;
+        printf("Your push index is %d and element is : %d\n",top,element);
+        st[top]=element;
+    }
+    return push;
+}
+void pop()
+{
+    if ( top == -1 ){
+        printf("Oops ! Stack is empty.\n");
+    }
+    else{
+    printf("Your popped element is : %d\n",st[top]);
+    top= top-1;
+    for(int n = top; n>=0; n-=1){
+
+
+    }
+    }
+}
+int main()
+{
+
+    int i;
+
+    printf("Enter the Stack size: ");
+    scanf("%d", &size);
+
+
+    printf("\n<<<< Here is your Stack >>>>\n\n");
+    printf("Your Stack size is : %d\n",size);
+
+    push(17);
+    push(5);
+    push(123);
+    push(25);
+    push(12);
+    push(87);
+
+    pop();
+    //pop();
+    //pop();
+
+
+
+    //peek();
+
+    for (i=top; i>=0; i-=1)
+    {
+        printf("%d\n",st[i]);
+    }
+
+
+
+
+}
